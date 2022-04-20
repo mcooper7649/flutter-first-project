c874bfe4e6009738bb4e6aacab8008cdd6a467e3
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-04-20 14:48:35 -0400

    raised buttons and anonymouse functions

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer chosen');
+  }
+
+  var questions = [
+    "What\'s your favorite color?",
+    "What\s your favorite animal?",
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +24,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: () => print('answer 2'),
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
